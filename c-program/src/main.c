cd0e822a8fed36aa867d44dea66d9023e8ba38c0
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2021-12-30 03:36:24 +0100

    Add a dbg_break to main. This makes it possible to inspect the core state before resuming.

--- c-program/src/main.c
+++ c-program/src/main.c
@@ -1,7 +1,10 @@
+extern void dbg_break();
+
 int main() {
   // volatile to avoid optimizing away the branch
   volatile int a = 1;
   volatile int b = 2;
+  dbg_break();
   if (a < b) {
     return 105;
   } else {
