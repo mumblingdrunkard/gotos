5a2abf2e3cf6c2a207b5d4e30f25653633257d19
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2021-12-30 03:35:44 +0100

    Rename exit.s to sys_exit.s to match name of function

--- c-program/src/sys_exit.s
+++ c-program/src/sys_exit.s
@@ -2,6 +2,7 @@
 .globl sys_exit
 .type sys_ext, @function
 
+// exit is special. It doesn't fill the return address as there should be no returing from the method
 sys_exit:
 	li    a7, 1 // syscall number
 	ecall
