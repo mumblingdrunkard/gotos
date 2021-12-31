4104dd6224fb4395e7988ffcacb74b86bcb48d5e
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2021-12-31 03:17:34 +0100

    Testing multiple cores sharing memory.

--- main.go
+++ main.go
@@ -15,25 +15,53 @@ func main() {
 	}
 	stats, err := f.Stat()
 	size := stats.Size()
-	program := make([]uint8, size)
-	binary.Read(f, binary.BigEndian, &program)
+	fib := make([]uint8, size)
+	binary.Read(f, binary.BigEndian, &fib)
+	f.Close()
+
+	f, err = os.Open("c-program/minimal.text")
+	if err != nil {
+		panic(err)
+	}
+	stats, err = f.Stat()
+	size = stats.Size()
+	minimal := make([]uint8, size)
+	binary.Read(f, binary.BigEndian, &minimal)
 	f.Close()
 
 	mem := memory.NewMemory(memory.LITTLE) // little endian memory
-	err, l := mem.Write(program, 0)
+	err, l := mem.Write(fib, 0)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Wrote %d bytes\n", l)
 
+	err, l = mem.Write(minimal, 1024*1024*2)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Wrote %d bytes\n", l)
+
+	// core1 runs the fib program
 	core1 := cpu.NewCoreWithMemory(&mem)
+	core1.UnsafeSetMemSize(1024 * 1024 * 2) // 2MiB for core1
+
+	// core2 runs the minimal program
+	core2 := cpu.NewCoreWithMemory(&mem)
+	core2.UnsafeSetMemBase(1024 * 1024 * 2) // core2 starts at 2MiB
+	core2.UnsafeSetMemSize(1024 * 1024 * 2) // 2MiB for core2
 
 	core1.StartAndWait()
+	core2.StartAndWait()
 
 	core1.Wait()
+	core2.Wait()
 
-	fmt.Printf("%d cycles\n", core1.Cycles())
-
+	fmt.Printf("core1: %d cycles\n", core1.Cycles())
 	core1.DumpRegisters()
+
+	fmt.Printf("core2: %d cycles\n", core2.Cycles())
+	core2.DumpRegisters()
 }
