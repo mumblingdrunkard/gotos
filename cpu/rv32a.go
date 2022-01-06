82f2b40732142ef0b89161d899736c916fcdf7bf
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-06 04:51:37 +0100

    update sets

--- cpu/rv32a.go
+++ cpu/rv32a.go
@@ -1,5 +1,7 @@
 package cpu
 
+import "fmt"
+
 func (c *Core) lr_w(inst uint32) {
 	rd := (inst >> 7) & 0x1f
 	rs1 := (inst >> 15) & 0x1f
@@ -10,9 +12,16 @@ func (c *Core) lr_w(inst uint32) {
 
 	// update rset
 	c.rsets.Lock()
-	_, c.reg[rd] = c.mc.LoadThroughWord(addr)
-	c.rsets.sets[c.id][pAddr] = true
+	c.mc.mem.Lock()
+	_, c.reg[rd] = c.mc.UnsafeLoadThroughWord(addr)
+	if c.reg[rd] == 0 {
+		fmt.Printf("%d\n", c.reg[rd])
+	}
+	c.mc.mem.Unlock()
+	m := c.rsets.lookup[c.id]
+	(*m)[pAddr] = true
 	c.rsets.Unlock()
+	// fmt.Println("LR.W success!")
 }
 
 func (c *Core) sc_w(inst uint32) {
@@ -28,13 +37,16 @@ func (c *Core) sc_w(inst uint32) {
 	c.rsets.Lock()
 
 	// check rset
-	if _, ok := c.rsets.sets[c.id][pAddr]; ok {
-		c.mc.StoreThroughWord(addr, c.reg[rs2])
+	if _, ok := (*c.rsets.lookup[c.id])[pAddr]; ok {
+		fmt.Println("SC.W success!")
+		c.mc.mem.Lock()
+		c.mc.UnsafeStoreThroughWord(addr, c.reg[rs2])
+		c.mc.mem.Unlock()
 
 		// invalidate entries on all harts
 		c.reg[rd] = 0
 		for i := range c.rsets.sets {
-			delete(c.rsets.sets[i], pAddr)
+			delete(*c.rsets.lookup[i], pAddr)
 		}
 	} else {
 		// failed
@@ -42,12 +54,31 @@ func (c *Core) sc_w(inst uint32) {
 	}
 
 	// Regardless of success or failure, executing an SC.W instruction invalidates any reservation held by this hart.
-	delete(c.rsets.sets[c.id], pAddr)
+	delete(*c.rsets.lookup[c.id], pAddr)
 
 	c.rsets.Unlock()
 }
 
 func (c *Core) amoswap_w(inst uint32) {
+	rd := (inst >> 7) & 0x1f
+	rs1 := (inst >> 15) & 0x1f
+	rs2 := (inst >> 20) & 0x1f
+
+	addr := c.reg[rs1]
+	src := c.reg[rs2]
+	_, pAddr, _ := c.mc.mmu.Translate(addr)
+
+	c.mc.mem.Lock()
+	_, c.reg[rd] = c.mc.UnsafeLoadThroughWord(addr)
+	c.mc.UnsafeStoreThroughWord(addr, src)
+	c.mc.mem.Unlock()
+
+	// Invalidate LRs
+	c.rsets.Lock()
+	for i := range c.rsets.sets {
+		delete(*c.rsets.lookup[i], pAddr)
+	}
+	c.rsets.Unlock()
 }
 
 func (c *Core) amoadd_w(inst uint32) {
