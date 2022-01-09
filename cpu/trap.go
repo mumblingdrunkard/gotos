ae87e10d4bbe9a9d83629d7d2c182841d42cd94f
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-09 16:08:44 +0100

    Renaming for namespace consintency

--- cpu/trap.go
+++ cpu/trap.go
@@ -5,39 +5,39 @@ type InterruptExceptionCode uint32
 
 // Exception codes when Interrupt is 1/true (see risc-v privileged spec table 8.6)
 const (
-	SUPERVISOR_SOFTWARE_INTERRUPT         InterruptExceptionCode = 1  // not used
-	VIRTUAL_SUPERVISOR_SOFTWARE_INTERRUPT                        = 2  // not used
-	MACHINE_SOFTWARE_INTERRUPT                                   = 3  // not used
-	SUPERVISOR_TIMER_INTERRUPT                                   = 5  // not used
-	VIRTUAL_SUPERVISOR_TIMER_INTERRUPT                           = 6  // not used
-	MACHINE_TIMER_INTERRUPT                                      = 7  // not used
-	SUPERVISOR_EXTERNAL_INTERRUPT                                = 9  // not used
-	VIRTUAL_SUPERVISOR_EXTERNAL_INTERRUPT                        = 10 // not used
-	MACHINE_EXTERNAL_INTERRUPT                                   = 11
-	SUPERVISOR_GUEST_EXTERNAL_INTERRUPT                          = 12 // not used
+	TRAP_SUPERVISOR_SOFTWARE_INTERRUPT         InterruptExceptionCode = 1  // not used
+	TRAP_VIRTUAL_SUPERVISOR_SOFTWARE_INTERRUPT                        = 2  // not used
+	TRAP_MACHINE_SOFTWARE_INTERRUPT                                   = 3  // not used
+	TRAP_SUPERVISOR_TIMER_INTERRUPT                                   = 5  // not used
+	TRAP_VIRTUAL_SUPERVISOR_TIMER_INTERRUPT                           = 6  // not used
+	TRAP_MACHINE_TIMER_INTERRUPT                                      = 7  // not used
+	TRAP_SUPERVISOR_EXTERNAL_INTERRUPT                                = 9  // not used
+	TRAP_VIRTUAL_SUPERVISOR_EXTERNAL_INTERRUPT                        = 10 // not used
+	TRAP_MACHINE_EXTERNAL_INTERRUPT                                   = 11
+	TRAP_SUPERVISOR_GUEST_EXTERNAL_INTERRUPT                          = 12 // not used
 )
 
 // Exception codes when Interrupt is 0/false (see risc-v privileged spec table 8.6)
 const (
-	INSTRUCTION_ADDRESS_MISALIGNED  ExceptionCode = 0
-	INSTRUCTION_ACCESS_FAULT                      = 1
-	ILLEGAL_INSTRUCTION                           = 2
-	BREAKPOINT                                    = 3
-	LOAD_ADDRESS_MISALIGNED                       = 2
-	LOAD_ACCESS_FAULT                             = 5
-	STORE_OR_AMO_ADDRESS_MISALIGNED               = 6
-	STORE_OR_AMO_ACCESS_FAULT                     = 7
-	ECALL_UMODE                                   = 8
-	ECALL_HSMODE                                  = 9  // not used
-	ECALL_VSMODE                                  = 10 // not used
-	ECALL_MMODE                                   = 11 // not used
-	INSTRUCTION_PAGE_FAULT                        = 12
-	LOAD_PAGE_FAULT                               = 13
-	STORE_OR_AMO_PAGE_FAULT                       = 15
-	INSTRUCTION_GUEST_PAGE_FAULT                  = 20
-	LOAD_GUEST_PAGE_FAULT                         = 21
-	VIRTUAL_INSTRUCTION                           = 22
-	STORE_AMO_GUEST_PAGE_FAULT                    = 23
+	TRAP_INSTRUCTION_ADDRESS_MISALIGNED  ExceptionCode = 0
+	TRAP_INSTRUCTION_ACCESS_FAULT                      = 1
+	TRAP_ILLEGAL_INSTRUCTION                           = 2
+	TRAP_BREAKPOINT                                    = 3
+	TRAP_LOAD_ADDRESS_MISALIGNED                       = 2
+	TRAP_LOAD_ACCESS_FAULT                             = 5
+	TRAP_STORE_OR_AMO_ADDRESS_MISALIGNED               = 6
+	TRAP_STORE_OR_AMO_ACCESS_FAULT                     = 7
+	TRAP_ECALL_UMODE                                   = 8
+	TRAP_ECALL_HSMODE                                  = 9  // not used
+	TRAP_ECALL_VSMODE                                  = 10 // not used
+	TRAP_ECALL_MMODE                                   = 11 // not used
+	TRAP_INSTRUCTION_PAGE_FAULT                        = 12
+	TRAP_LOAD_PAGE_FAULT                               = 13
+	TRAP_STORE_OR_AMO_PAGE_FAULT                       = 15
+	TRAP_INSTRUCTION_GUEST_PAGE_FAULT                  = 20
+	TRAP_LOAD_GUEST_PAGE_FAULT                         = 21
+	TRAP_VIRTUAL_INSTRUCTION                           = 22
+	TRAP_STORE_AMO_GUEST_PAGE_FAULT                    = 23
 )
 
 func (c *Core) trap(interrupt bool, icode InterruptExceptionCode, ecode ExceptionCode) {
