1b554f16147c458c63b5317b7e1a476fd6e2158f
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-12 00:35:47 +0100

    Don't export unused trap reasons

--- cpu/trap.go
+++ cpu/trap.go
@@ -1,40 +1,39 @@
 package cpu
 
-type ExceptionCode uint32
-type InterruptExceptionCode uint32
-
-// Exception codes when Interrupt is 1/true (see risc-v privileged spec table 8.6)
-const (
-	TRAP_SUPERVISOR_SOFTWARE_INTERRUPT         InterruptExceptionCode = 1  // not used
-	TRAP_VIRTUAL_SUPERVISOR_SOFTWARE_INTERRUPT                        = 2  // not used
-	TRAP_MACHINE_SOFTWARE_INTERRUPT                                   = 3  // not used
-	TRAP_SUPERVISOR_TIMER_INTERRUPT                                   = 5  // not used
-	TRAP_VIRTUAL_SUPERVISOR_TIMER_INTERRUPT                           = 6  // not used
-	TRAP_MACHINE_TIMER_INTERRUPT                                      = 7  // not used
-	TRAP_SUPERVISOR_EXTERNAL_INTERRUPT                                = 9  // not used
-	TRAP_VIRTUAL_SUPERVISOR_EXTERNAL_INTERRUPT                        = 10 // not used
-	TRAP_MACHINE_EXTERNAL_INTERRUPT                                   = 11
-)
+type TrapReason uint32
 
 // Exception codes when Interrupt is 0/false (see risc-v privileged spec table 8.6)
 const (
-	TRAP_INSTRUCTION_ADDRESS_MISALIGNED  ExceptionCode = 0
-	TRAP_INSTRUCTION_ACCESS_FAULT                      = 1
-	TRAP_ILLEGAL_INSTRUCTION                           = 2
-	TRAP_BREAKPOINT                                    = 3
-	TRAP_LOAD_ADDRESS_MISALIGNED                       = 4
-	TRAP_LOAD_ACCESS_FAULT                             = 5
-	TRAP_STORE_OR_AMO_ADDRESS_MISALIGNED               = 6
-	TRAP_STORE_OR_AMO_ACCESS_FAULT                     = 7
-	TRAP_ECALL_UMODE                                   = 8
-	TRAP_ECALL_HSMODE                                  = 9  // not used
-	TRAP_ECALL_VSMODE                                  = 10 // not used
-	TRAP_ECALL_MMODE                                   = 11 // not used
-	TRAP_INSTRUCTION_PAGE_FAULT                        = 12
-	TRAP_LOAD_PAGE_FAULT                               = 13
-	TRAP_STORE_OR_AMO_PAGE_FAULT                       = 15
+	// Exception reasons
+	TrapInstructionAddressMisaligned TrapReason = 0x000
+	TrapInstructionAccessFault                  = 0x001
+	TrapIllegalInstruction                      = 0x002
+	TrapBreakpoint                              = 0x003
+	TrapLoadAddressMisaligned                   = 0x004
+	TrapLoadAccessFault                         = 0x005
+	TrapStoreAddressMisaligned                  = 0x006
+	TrapStoreAccessFault                        = 0x007
+	TrapEcallUMode                              = 0x008
+	trapEcallHSMode                             = 0x009 // not used
+	trapEcallVSMode                             = 0x00A // not used
+	trapEcallMMode                              = 0x00B // not used
+	TrapInstructionPageFault                    = 0x00C
+	TrapLoadPageFault                           = 0x00D
+	TrapStorePageFault                          = 0x00F
+
+	// Interrupt reasons
+	trapSupervisorSoftwareInterrupt        = 0x801 // not used
+	trapVirtualSupervisorSoftwareInterrupt = 0x802 // not used
+	trapMachineSoftwareInterrupt           = 0x803 // not used
+	trapSupervisorTimerInterrupt           = 0x805 // not used
+	trapVirtualSupervisorTimerInterrupt    = 0x806 // not used
+	TrapMachineTimerInterrupt              = 0x807
+	trapSupervisorExternalInterrupt        = 0x809 // not used
+	trapVirtualSupervisorExternalInterrupt = 0x80A // not used
+	TrapMachineExternalInterrupt           = 0x80B
 )
 
-func (c *Core) trap(interrupt bool, icode InterruptExceptionCode, ecode ExceptionCode) {
+func (c *Core) trap(reason TrapReason) {
 	// TODO
+	c.trapFn(c, reason)
 }
