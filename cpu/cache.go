91d37856f9fcc81f3a3bfdea4b128cde173c3032
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-11 23:18:42 +0100

    Add some documentation comments, remove debug prints

--- cpu/cache.go
+++ cpu/cache.go
@@ -1,3 +1,7 @@
+// This file contains implementation of Cache and methods to access it.
+// Caching helps reduce lock contention when multiple processors are running.
+// This introduces the problem of cache coherence, which is creatively solved in other ways.
+
 package cpu
 
 import (
@@ -7,22 +11,23 @@ import (
 
 // TODO check alignments
 
+// Cache constants
 const (
-	CACHE_LINE_LENGTH      = 64
-	CACHE_LINE_OFFSET_BITS = 6
-	CACHE_LINE_OFFSET_MASK = 0x3F
-	CACHE_LINE_COUNT       = 64
+	CACHE_LINE_LENGTH      = 64   // must be equal to 2^CACHE_LINE_OFFSET_BITS (^ being power, not xor)
+	CACHE_LINE_OFFSET_BITS = 6    // how many bits of the address are used for the offset within a cache-line
+	CACHE_LINE_OFFSET_MASK = 0x3F // used to extract the offset in a cache line from an address
+	CACHE_LINE_COUNT       = 64   // how many cache lines the cache contains by default
 )
 
+// Cache flags
 const (
-	CACHE_F_NONE  uint8 = 0x00
-	CACHE_F_ALL         = 0xFF
-	CACHE_F_DIRTY       = 0x01
-	CACHE_F_STALE       = 0x02
+	CACHE_F_NONE  uint8 = 0x00 // no flags
+	CACHE_F_ALL         = 0xFF // all flags
+	CACHE_F_DIRTY       = 0x01 // dirty flag - triggers a cache flush when replaced
+	CACHE_F_STALE       = 0x02 // stale flag - triggers/or should trigger a refresh when a stale line is accessed
 )
 
-// TODO Cache read/write permission flags
-
+// A CacheLine contains
 type CacheLine struct {
 	number uint32
 	flags  uint8
@@ -38,6 +43,9 @@ type Cache struct {
 	endian Endian
 }
 
+// Attempts to load a byte (uint8) from cache.
+// If the cache line containing the byte is present and not marked stale, returns
+// (true, byte), oherwise (false, 0).
 func (c *Cache) LoadByte(address uint32) (present bool, byte uint8) {
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
 	if line, present := c.lookup[lineNumber]; present {
@@ -51,6 +59,9 @@ func (c *Cache) LoadByte(address uint32) (present bool, byte uint8) {
 	return false, 0
 }
 
+// Attempts to load a halfword (uint16) from cache.
+// If the cache line containing the halfword is present and not marked stale, returns
+// (true, halfword), oherwise (false, 0).
 func (c *Cache) LoadHalfWord(address uint32) (bool, uint16) {
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
 	if line, present := c.lookup[lineNumber]; present {
@@ -68,6 +79,9 @@ func (c *Cache) LoadHalfWord(address uint32) (bool, uint16) {
 	return false, 0
 }
 
+// Attempts to load a word (uint32) from cache.
+// If the cache line containing the word is present and not marked stale, returns
+// (true, word), oherwise (false, 0).
 func (c *Cache) LoadWord(address uint32) (bool, uint32) {
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
 	if line, present := c.lookup[lineNumber]; present {
@@ -85,6 +99,9 @@ func (c *Cache) LoadWord(address uint32) (bool, uint32) {
 	return false, 0
 }
 
+// Attempts to load a doubleword (uint64) from cache.
+// If the cache line containing the doubleword is present and not marked stale, returns
+// (true, doubleword), oherwise (false, 0).
 func (c *Cache) LoadDoubleWord(address uint32) (bool, uint64) {
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
 	if line, present := c.lookup[lineNumber]; present {
@@ -102,6 +119,9 @@ func (c *Cache) LoadDoubleWord(address uint32) (bool, uint64) {
 	return false, 0
 }
 
+// Attempts to store a byte (uint8) to cache.
+// If the cache line containing the byte is present and not marked stale, stores
+// the byte and returns true, false otherwise.
 func (c *Cache) StoreByte(address uint32, b uint8) bool {
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
 	if line, present := c.lookup[lineNumber]; present {
@@ -117,6 +137,9 @@ func (c *Cache) StoreByte(address uint32, b uint8) bool {
 	return false
 }
 
+// Attempts to store a halfword (uint16) to cache.
+// If the cache line containing the halfword is present and not marked stale, stores
+// the halfword and returns true, false otherwise.
 func (c *Cache) StoreHalfWord(address uint32, hw uint16) bool {
 	// TODO
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
@@ -139,6 +162,9 @@ func (c *Cache) StoreHalfWord(address uint32, hw uint16) bool {
 	return false
 }
 
+// Attempts to store a word (uint32) to cache.
+// If the cache line containing the word is present and not marked stale, stores
+// the word and returns true, false otherwise.
 func (c *Cache) StoreWord(address uint32, w uint32) bool {
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
 	if line, present := c.lookup[lineNumber]; present {
@@ -160,6 +186,9 @@ func (c *Cache) StoreWord(address uint32, w uint32) bool {
 	return false
 }
 
+// Attempts to store a doubleword (uint32) to cache.
+// If the cache line containing the doubleword is present and not marked stale, stores
+// the doubleword and returns true, false otherwise.
 func (c *Cache) StoreDoubleWord(address uint32, dw uint64) bool {
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
 	if line, present := c.lookup[lineNumber]; present {
@@ -181,6 +210,10 @@ func (c *Cache) StoreDoubleWord(address uint32, dw uint64) bool {
 	return false
 }
 
+// Attempts to store a word (uint32) to cache, but does not set the dirty bit if
+// the store is completed.
+// If the cache line containing the word is present and not marked stale, stores
+// the word and returns true, false otherwise.
 func (c *Cache) StoreWordNoDirty(address uint32, w uint32) bool {
 	lineNumber := address >> CACHE_LINE_OFFSET_BITS
 	if line, present := c.lookup[lineNumber]; present {
@@ -201,7 +234,22 @@ func (c *Cache) StoreWordNoDirty(address uint32, w uint32) bool {
 	return false
 }
 
+// Loads a cache-line into cache.
+// Behaviour depends on the state of the cache.
+// If the cache is not full: selects the next open slot in the line-storage and
+// loads the cache-line into it.
+// This is done by copying the line data from `src` (usually mc.mem)
+//
+// If the cache-line is already present, but it is stale: refreshes that line.
+//
+// If the cache is full and the cache-line is not present, selects a random line
+// to be evicted.
+// If this line is dirty, the line is first flushed (written back to `src`) before
+// it is replaced with the new data.
+//
+// Returns true if a replacement/refresh was performed, false otherwise.
 func (c *Cache) ReplaceRandom(lineNumber uint32, flags uint8, src []uint8) bool {
+	// if line is already present
 	if _, present := c.lookup[lineNumber]; present {
 		line := c.lookup[lineNumber]
 
@@ -229,7 +277,6 @@ func (c *Cache) ReplaceRandom(lineNumber uint32, flags uint8, src []uint8) bool 
 	delete(c.lookup, eject.number) // delete the old entry, no-op if there is no old entry
 
 	// check if it's dirty, if so, flush
-	// if the cache isn't full, this shouldn'd be dirty
 	if eject.flags&CACHE_F_DIRTY != 0 {
 		copy(src[address:], eject.data[:])
 	}
@@ -237,10 +284,6 @@ func (c *Cache) ReplaceRandom(lineNumber uint32, flags uint8, src []uint8) bool 
 	// writes the line to cache
 	address = lineNumber << CACHE_LINE_OFFSET_BITS
 	copy(eject.data[:], src[address:address+CACHE_LINE_LENGTH])
-	// fmt.Println("FRESH CACHE LINE")
-	// for i := 0; i < 64; i += 4 {
-	// 	fmt.Printf("%08X\n", binary.LittleEndian.Uint32(eject.data[i:i+4]))
-	// }
 	eject.flags = flags
 	eject.number = lineNumber
 
@@ -250,6 +293,8 @@ func (c *Cache) ReplaceRandom(lineNumber uint32, flags uint8, src []uint8) bool 
 	return true
 }
 
+// Flushes all cache-lines back to `src`.
+// Helpful if you want all other cores to see changes made by this core.
 func (c *Cache) FlushAll(src []uint8) int {
 	flushed := 0
 	for i := range c.lines {
@@ -268,6 +313,8 @@ func (c *Cache) FlushAll(src []uint8) int {
 	return flushed
 }
 
+// Marks all cache-lines as stale.
+// Helpful if you want to guarantee that you can see updates made from other cores.
 func (c *Cache) InvalidateAll() {
 	for i := range c.lines {
 		line := &c.lines[i]
