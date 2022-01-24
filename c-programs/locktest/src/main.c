24bee814346c4c8367dd1d18c277af119bdd1988
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-24 03:33:06 +0100

    Add some notes in locktest about performance when using mutexes.

--- c-programs/locktest/src/main.c
+++ c-programs/locktest/src/main.c
@@ -1,6 +1,6 @@
 extern void dbg_break();
 extern int sys_id();
-extern void atomic_add(int *addr, int val);
+extern void atomic_add(int *, int);
 
 #include "mutex.h"
 
@@ -9,6 +9,25 @@ int main() {
     int *ct = (int *)2048;
 
     for (int i = 0; i < 1024 * 32; i++) {
+        // This really sucks for cache and we're only updating one counter so
+        // the performance is pure garbage. An improvement would be to use one
+        // of the instructions from the A extension such as AMOADD. This
+        // instruction is available as the function `atomic_add(...)` which
+        // takes a pointer to an int and a value to increment the number that
+        // this pointer points to.
+        //
+        // Example code
+        //
+        //     ```c
+        //     atomic_add(ct, 1);
+        //     ```
+        //
+        //     Code example: Using `atomic_add` to increment a counter `ct`.
+        //
+        // is the equivalent, thread-safe method to increment the counter ct
+        // by 1, but has much higher performance because it doesn't thrash the
+        // cache and doesn't require locking and unlocking.
+
         lock(m);
         (*ct)++;
         unlock(m);
