009309985e29b92a5f9175dbc6d949fbfbbc0f9a
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-26 19:16:22 +0100

    Add and update a bunch of flags. Remove plans for segmentation

--- cpu/mmu.go
+++ cpu/mmu.go
@@ -3,36 +3,23 @@ package cpu
 // TODO Should mmu be moved into its own package perhaps?
 
 const (
-	memFlagRead    uint8 = 0x01 // indicates that the processor is allowed to read data from this address
-	memFlagWrite         = 0x02 // indicates that the processor is allowed to write data to this address
-	memFlagExec          = 0x04 // indicates that the processor is allowed to fetch instructions from this address
-	memFlagNoCache       = 0x08 // if I ever get around to doing MMIO
+	mmuFlagRead    uint8 = 0x01 // indicates that the processor is allowed to read data from this address
+	mmuFlagWrite         = 0x02 // indicates that the processor is allowed to write data to this address
+	mmuFlagExec          = 0x04 // indicates that the processor is allowed to fetch instructions from this address
+	mmuFlagPresent       = 0x08 // the virtual address maps to a physical one, if this flag is 0, the returned address should be 0
+	mmuFlagValid         = 0x10 // the virtual address is valid
+	mmuFlagNoCache       = 0x20 // if I ever get around to doing MMIO
 )
 
-//
-// Very simple mmu
-// MMU likely requires at least 4 base-bound pairs for general purpose use, but likely should have 8 or more.
-// The segments I propose are:
-//
-// Name       | Flag | Purpose
-// -----------|------|--------
-// SegProgram | X    | Instruction memory
-// SegData    |  R   | Read only data
-// SegHeap    |  RW  | Program heap (can explicitly be resized by the program)
-// SegStack   |  RW  | Program stack (should imlicitly grow on demand)
-//
-// This layout does not allow for uncached I/O and it would have to be done all through syscalls.
-//
-// Paging is the better option, but I think students should be gradually introduced to the concepts
-// Might even do well with just a single segment at first, then two, then four, etc. etc..
-//
+type page struct {
+	frameNumber uint32
+	flags       uint8
+}
 
-type segment struct {
-	base      uint32
-	size      uint32
-	allocated uint32
-	flags     uint8
-	backwards bool
+// `NNNNNFF0` where NNNNN is the physical frame number that it maps to and FF are the memory flags
+type tlbEntry uint32
+
+type tlb struct {
 }
 
 type mmu struct {
@@ -44,11 +31,11 @@ type mmu struct {
 // This method should return (false, false, 0, 0) when the address is invalid
 // If a translation is valid, but the page is missing (when paging is implemented), the function should return (true, false, vAddr, flags)
 // If a translation is valid and the page is present, the function should return (true, true, vAddr, flags)
-func (m *mmu) translateAndCheck(vAddr uint32) (hit, valid, present bool, pAddr uint32, flags uint8) {
-	if pAddr >= m.size {
-		return true, false, false, 0, 0
+func (c *Core) translateAndCheck(vAddr uint32) (hit bool, pAddr uint32, flags uint8) {
+	if pAddr >= c.mc.mmu.size {
+		return true, 0, 0
 	}
-	return true, true, true, vAddr + m.base, memFlagRead | memFlagWrite | memFlagExec
+	return true, vAddr + c.mc.mmu.base, mmuFlagRead | mmuFlagWrite | mmuFlagExec | mmuFlagPresent
 }
 
 func newMMU() mmu {
