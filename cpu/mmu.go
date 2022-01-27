95b13cef901d26fee762e8a5ccf738a2bf25de21
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-27 07:48:13 +0100

    Large changes to mmu flags to reflect those found in the RISC-V privileged specification chapter 4.3

--- cpu/mmu.go
+++ cpu/mmu.go
@@ -3,23 +3,34 @@ package cpu
 // TODO Should mmu be moved into its own package perhaps?
 
 const (
-	mmuFlagRead    uint8 = 0x01 // indicates that the processor is allowed to read data from this address
-	mmuFlagWrite         = 0x02 // indicates that the processor is allowed to write data to this address
-	mmuFlagExec          = 0x04 // indicates that the processor is allowed to fetch instructions from this address
-	mmuFlagPresent       = 0x08 // the virtual address maps to a physical one, if this flag is 0, the returned address should be 0
-	mmuFlagValid         = 0x10 // the virtual address is valid
-	mmuFlagNoCache       = 0x20 // if I ever get around to doing MMIO
+	mmuFlagValid       uint8 = 0x01 // the virtual address is valid
+	mmuFlagRead              = 0x02 // indicates that the processor is allowed to read data from this address
+	mmuFlagWrite             = 0x04 // indicates that the processor is allowed to write data to this address
+	mmuFlagExec              = 0x08 // indicates that the processor is allowed to fetch instructions from this address
+	mmuFlagUModeAccess       = 0x10
+	mmuFlagGlobal            = 0x20
+	mmuFlagAccessed          = 0x40
+	mmuFlagDirty             = 0x80
 )
+
+// Two schemes to manage the A and D bits are permitted:
+// - When a virtual page is accessed and the A bit is clear, or is written and
+//   the D bit is clear, a page-fault exception is raised.
+// - ...
+//
+// ...
+//
+//     ---
+//         The A and D bits are never cleared by the implementation.
+//     If the supervisor software does not rely on accessed and/or dirty bits,
+//     e.g. if it does not swap memory pages to secondary storage or if the
+//     pages are being used to map I/O space, it should always set them to 1 in
+//     the PTE to improve performance.
+//     ---
 
 type page struct {
 	frameNumber uint32
 	flags       uint8
-}
-
-// `NNNNNFF0` where NNNNN is the physical frame number that it maps to and FF are the memory flags
-type tlbEntry uint32
-
-type tlb struct {
 }
 
 type mmu struct {
@@ -35,7 +46,7 @@ func (c *Core) translateAndCheck(vAddr uint32) (hit bool, pAddr uint32, flags ui
 	if pAddr >= c.mc.mmu.size {
 		return true, 0, 0
 	}
-	return true, vAddr + c.mc.mmu.base, mmuFlagRead | mmuFlagWrite | mmuFlagExec | mmuFlagPresent
+	return true, vAddr + c.mc.mmu.base, mmuFlagValid | mmuFlagRead | mmuFlagWrite | mmuFlagExec
 }
 
 func newMMU() mmu {
