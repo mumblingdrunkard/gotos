c45cf7e87d2ca636bcfc9e81f812d7acef2daec1
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-29 03:27:39 +0100

    Add a second core to run another instance of the given program.

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	// TODO create queue of PCBs with programs loaded in memory, then pass this
 	// off to the system to handle instead of starting the core at some random address.
-	f, err := os.Open("c-programs/locktest/locktest.text")
+	f, err := os.Open("c-programs/fib/fib.text")
 	if err != nil {
 		panic(err)
 	}
@@ -29,26 +29,46 @@ func main() {
 		panic(err)
 	}
 
+	err, l = mem.Write(0x14000, fib)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Printf("Wrote %d bytes\n", l)
 
-	rs := cpu.NewReservationSets(1)
+	rs := cpu.NewReservationSets(2)
 
 	core0 := cpu.NewCoreWithMemoryAndReservationSets(&mem, &rs, 0)
+	core1 := cpu.NewCoreWithMemoryAndReservationSets(&mem, &rs, 1)
 
-	core0.SetPC(0x4000)                        // TODO remove this
-	core0.SetIRegister(cpu.Reg_SP, mem.Size()) // TODO currently sets the stack pointer (stored in SP) to the last byte in the memory
+	core0.SetPC(0x4000) // TODO remove this
+	core0.SetIRegister(cpu.Reg_SP, 0x8000)
+
+	core1.SetPC(0x14000) // TODO remove this
+	core1.SetIRegister(cpu.Reg_SP, 0x18000)
 
 	core0.SetBootHandler(system.SystemStartup)
 	core0.SetTrapHandler(system.TrapHandler)
 
+	core1.SetBootHandler(system.SystemStartup)
+	core1.SetTrapHandler(system.TrapHandler)
+
 	var wg sync.WaitGroup
 	core0.StartAndSync(&wg)
+	core1.StartAndSync(&wg)
 
 	core0.Wait()
+	core1.Wait()
 
 	core0.DumpRegisters()
 	fmt.Println("Performance statistics:")
 	fmt.Printf("\ncore0: %d cycles\n", core0.InstructionsRetired())
 	fmt.Printf("core0: %d misses\n", core0.Misses())
 	fmt.Printf("core0: %d accesses\n", core0.Accesses())
+
+	core1.DumpRegisters()
+	fmt.Println("Performance statistics:")
+	fmt.Printf("\ncore1: %d cycles\n", core1.InstructionsRetired())
+	fmt.Printf("core1: %d misses\n", core1.Misses())
+	fmt.Printf("core1: %d accesses\n", core1.Accesses())
 }
