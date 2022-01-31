727f12cd9582288e157c17b0da753bccded0dffb
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-01-31 04:03:57 +0100

    Cleanup

--- c-programs/locktest/src/_start.c
+++ c-programs/locktest/src/_start.c
@@ -1,4 +1,5 @@
 #include "sys.h"
 
 extern int main();
+
 void _Noreturn _start() { sys_exit(main()); }
--- c-programs/locktest/src/main.c
+++ c-programs/locktest/src/main.c
@@ -1,28 +1,14 @@
-#include "sys.h"
 #include "mutex.h"
 
-extern void atomic_add(int *, int);
-
 int main() {
-    char s[] = "Tello, World!";
-    s[0] = 'H';
-
     struct mutex *m = (struct mutex *)(0x20000);
     int *ct = (int *)(0x21000);
-
-    putint((unsigned int)s);
-    print(s, 13);
-
-    int id = getpid();
-    putint(id);
 
     for (int i = 0; i < 1024 * 32; i++) {
         lock(m);
         (*ct)++;
         unlock(m);
     }
-
-    putint(id);
 
     lock(m);
     int ret = (*ct);
