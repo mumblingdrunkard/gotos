fa35036b8949258ce29a68f4d6814883a52efc32
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-02-06 16:32:05 +0100

    Don't set PC in boot-handler

--- system/boot_handler.go
+++ system/boot_handler.go
@@ -6,6 +6,5 @@ import "gotos/cpu"
 // This function should set up all registers and whatnot to prepare the core to start running programs.
 
 func (s *System) HandleBoot(c *cpu.Core) {
-	c.SetPC(0)
 	c.SetIRegister(cpu.Reg_SP, cpu.MemorySize)
 }
