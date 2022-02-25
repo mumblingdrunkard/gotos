f1752bcfeba327f5829dd2ce7c2da571856907b6
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-02-25 21:48:07 +0100

    rename vpn to vpi

--- cpu/tlb.go
+++ cpu/tlb.go
@@ -1,7 +1,8 @@
 package cpu
 
 const (
-	tlbSize         = 256
+	tlbSize = 256
+	// valid vpis will never have the leftmost bit set
 	tlbInvalidEntry = 0xFFFFFFFFFFFFFFFF
 	tlbTryDepth     = 2
 )
@@ -10,31 +11,31 @@ type tlb struct {
 	entries [tlbSize]uint64
 }
 
-func (t *tlb) load(vpn uint32) (bool, uint32) {
+func (t *tlb) load(vpi uint32) (bool, uint32) {
 	for i := uint32(0); i < tlbTryDepth; i++ {
-		v := t.entries[(vpn+i*i)&0xFF]
-		if uint32(v>>32) == vpn {
+		v := t.entries[(vpi+i*i)&0xFF]
+		if uint32(v>>32) == vpi {
 			return true, uint32(v)
 		}
 	}
 	return false, 0
 }
 
-func (t *tlb) store(vpn, pte uint32) bool {
+func (t *tlb) store(vpi, pte uint32) bool {
 	for i := uint32(0); i < tlbTryDepth; i++ {
-		v := t.entries[(vpn+i*i)&0xFF]
+		v := t.entries[(vpi+i*i)&0xFF]
 		if v == tlbInvalidEntry {
-			t.entries[(vpn+i*i)&0xFF] = (uint64(vpn) << 32) | uint64(pte)
+			t.entries[(vpi+i*i)&0xFF] = (uint64(vpi) << 32) | uint64(pte)
 			return true
 		}
 	}
 
 	// if all are filled, invalidate all entries
 	for i := uint32(0); i < tlbTryDepth; i++ {
-		t.entries[(vpn+i*i)&0xFF] = tlbInvalidEntry
+		t.entries[(vpi+i*i)&0xFF] = tlbInvalidEntry
 	}
 
-	t.entries[vpn&0xFF] = (uint64(vpn) << 32) | uint64(pte)
+	t.entries[vpi&0xFF] = (uint64(vpi) << 32) | uint64(pte)
 	return true
 }
 
