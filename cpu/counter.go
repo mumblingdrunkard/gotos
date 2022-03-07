94ac35ccc07b35ae4876c06cb09ee7167be3f2b9
mumblingdrunkard <mumblingdrunkard@protonmail.com>
2022-03-07 05:32:21 +0100

    remove enablecounter, enable counter in setcounter

--- cpu/counter.go
+++ cpu/counter.go
@@ -5,10 +5,12 @@ type counter struct {
 	value  uint64
 }
 
-func (c *Core) EnableCounter() {
-	c.counter.enable = true
-}
-
+// lowest valid value for the counter should be 32
+// 0 would make the counter underflow in the next cycle
+// 1 would make the counter trigger in the next cycle
+// 2 lets the processor make process (albeit slowly because of excessive interrupts) in most cases
+// 32 guarantees that LR/SC loops can make progress as well
 func (c *Core) SetCounter(v uint64) {
+	c.counter.enable = true
 	c.counter.value = v
 }
